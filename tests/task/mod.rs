5f461c20b03e82170169f45f066f3559bca56e44
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 21:39:01 +0800

    test: update tests for executor, service, and task modules

--- tests/task/mod.rs
+++ tests/task/mod.rs
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 mod task_execution_error_tests;
 mod task_handle_tests;
 mod task_runner_tests;
